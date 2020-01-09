e0476f2da75e975b5e4c814308e09f4bdea8c6ac
Boris Melikian <boris@lariushin.org>
2020-01-09 20:12:59 +0200

    2012

--- res/jgconfig.js
+++ res/jgconfig.js
@@ -2,21 +2,21 @@ $('#pl-fthumbs').justifiedGallery({
 	lastRow: 'center',
 	margins: 8,
 	border: 20,
-	rowHeight: 405,
-	maxRowHeight: 540,
+	rowHeight: 225,
+	maxRowHeight: 300,
 	cssAnimation: false,
 	refreshSensitivity: 18,
 	waitThumbnailsLoad: false,
-	captionSettings: {animationDuration: 1000, visibleOpacity: 0.7, nonVisibleOpacity: 0.7}
+	captionSettings: {animationDuration: 1000, visibleOpacity: 0.7, nonVisibleOpacity: 0.0}
 });
 $('#pl-thumbs').justifiedGallery({
 	lastRow: 'center',
 	margins: 8,
 	border: 20,
-	rowHeight: 270,
-	maxRowHeight: 360,
+	rowHeight: 180,
+	maxRowHeight: 240,
 	cssAnimation: false,
 	refreshSensitivity: 18,
 	waitThumbnailsLoad: false,
-	captionSettings: {animationDuration: 1000, visibleOpacity: 0.7, nonVisibleOpacity: 0.7}
+	captionSettings: {animationDuration: 1000, visibleOpacity: 0.7, nonVisibleOpacity: 0.0}
 });
